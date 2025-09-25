5b04ac5bf59b51c5adafa0212420fc5b4963935f
Raghav Sachdev <iraghavsachdev@gmail.com>
2025-09-25 11:53:34 +0530

    Fixes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,12 +5,6 @@ export default function TabLayout() {
   return (
     <Stack initialRouteName="sites">
       <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
         name="sites"
         options={{
           headerShown: false,
